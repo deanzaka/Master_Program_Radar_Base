418a328084da8e7daead550dd4bc110f248f9992
deanzaka <deanzaka.h@gmail.com>
2014-06-17 17:24:26 +0700

    Ganti semua, yg ini pake atmega162, usartnya 2 biji

--- motor.c
+++ motor.c
@@ -4,34 +4,30 @@
 void motor_set()
 {
  
-PORTD.2 = 0;
-PORTD.3 = 0;
-PORTD.4 = 0;
+
 PORTD.5 = 0;
 PORTD.6 = 0;
 PORTD.7 = 0;
 
-DDRD.2 = 1;
-DDRD.3 = 1;
-DDRD.4 = 1;
+
 DDRD.5 = 1;
 DDRD.6 = 1;
 DDRD.7 = 1;
 
 // Timer/Counter 1 initialization
 // Clock source: System Clock
-// Clock value: 10.800 kHz
-// Mode: Fast PWM top=00FFh
+// Clock value: 11059,200 kHz
+// Mode: Fast PWM top=0x03FF
 // OC1A output: Non-Inv.
 // OC1B output: Non-Inv.
 // Noise Canceler: Off
 // Input Capture on Falling Edge
-// Timer 1 Overflow Interrupt: Off
+// Timer1 Overflow Interrupt: Off
 // Input Capture Interrupt: Off
 // Compare A Match Interrupt: Off
 // Compare B Match Interrupt: Off
-TCCR1A=0xA1;
-TCCR1B=0x0D;
+TCCR1A=0xA3;
+TCCR1B=0x09;
 TCNT1H=0x00;
 TCNT1L=0x00;
 ICR1H=0x00;
--- motor.h
+++ motor.h
@@ -1,12 +1,9 @@
 #ifndef _MOTOR_INCLUDED_
 #define _MOTOR_INCLUDED_
 
-#define PWM_R OCR1B
-#define CS0_R PORTD.3
-#define CS1_R PORTD.2 
-#define PWM_L OCR1A
-#define CS0_L PORTD.6
-#define CS1_L PORTD.7
+#define PWM_R OCR1A
+#define CS0_R PORTD.6
+#define CS1_R PORTD.7
 
 #define CW        1
 #define CCW       2 
